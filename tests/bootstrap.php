cc9c9ba959be9b2a2274ad7aba81246698ba62a3
Vincent Letourneau <nanoninja.inc@gmail.com>
2014-04-01 10:36:42 +0200

    Changing configuration

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2014 The Elefanto Authors. All rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * licence that can be found in the LICENCE file.
+ */
+
+define('DS', DIRECTORY_SEPARATOR);
+require dirname(__DIR__).DS.'vendor'.DS.'autoload.php';
+date_default_timezone_set('UTC');
