31c9950507f962d614d200ef6736bb890668c8c5
Vincent Letourneau <nanoninja.inc@gmail.com>
2014-04-06 22:17:23 +0200

    Adding Standard Collection

---
+++ src/Elefanto/Std/Collection.php
@@ -0,0 +1,62 @@
+<?php
+/* Copyright 2014 The Elefanto Authors. All rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * licence that can be found in the LICENCE file.
+ */
+
+namespace Elefanto\Std;
+
+class Collection
+{
+    /**
+     * Test whether an array is an array list
+     * 
+     * For example:
+     * <code>
+     * $list = ['foo', 'bar'];
+     * $list = array(
+     *     '1' => 'a',
+     *     '2' => 'b',
+     *      3  => 'c',
+     * );
+     * </code>
+     * 
+     * @param  mixed $value
+     * @return bool
+     */
+    public function isList($value)
+    {
+        if (!is_array($value)) {
+            return false;
+        }
+
+        return count(array_filter(array_keys($value), 'is_int')) === count($value);
+    }
+
+    /**
+     * Test whether an array is a map.
+     * 
+     * For example:
+     * <code>
+     * $map = array(
+     *     'city'    => 'London',
+     *     'country' => 'England',
+     * );
+     * $map = array(
+     *     'type' => 'assoc',
+     *     'key'  => 'value',
+     * );
+     * </code>
+     * 
+     * @param  mixed $value
+     * @return bool
+     */
+    public function isMap($value)
+    {
+        if (!is_array($value)) {
+            return false;
+        }
+
+        return count(array_filter(array_keys($value), 'is_string')) === count($value);
+    }
+}
