6a6406d96a425cad97dec9f6606326c66d70b6e3
Vincent Letourneau <nanoninja.inc@gmail.com>
2014-04-11 10:31:06 +0200

    The name of the class Std\Collection has been modified for Std\ArrayTools

--- src/Elefanto/Std/ArrayTools.php
+++ src/Elefanto/Std/ArrayTools.php
@@ -7,8 +7,43 @@
 
 namespace Elefanto\Std;
 
-class Collection
+class ArrayTools
 {
+    /**
+     * Convert a simple array list to map (assoc) 
+     *
+     * For example:
+     * <code>
+     * $list = array('A', 'B', 'C', 'D');
+     * $map = array('A' => 'B', 'C' => 'D');
+     *
+     * $list = array('A', 'B', 'C');
+     * $map = array('A' => 'B', 'C' => null);
+     * </code>
+     *
+     * @param  array $list
+     * @return array
+     */
+    public static function listToMap(array $list)
+    {
+        $keys = $values = array();
+        $count = count($list);
+
+        for ($i = 0; $i < $count; $i++) {
+            if ($i % 2 == 0) {
+                $keys[] = $list[$i];
+            } else {
+                $values[] = $list[$i];
+            }
+        }
+
+        if ($count % 2 != 0) {
+            $values[] = null;
+        }
+
+        return array_combine($keys, $values);
+    }
+
     /**
      * Test whether an array is an array list
      * 
